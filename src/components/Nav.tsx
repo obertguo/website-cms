1f74839fd8e9f02658547d1aa936687457b19640
obertguo <obertguo@gmail.com>
2021-09-23 10:26:57 -0400

    innit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () =>{
+    return(
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Google Drive Notes</a>
+            <a href="#">Articles</a>
+            <a href="#">Contact</a>
+        </nav>
+    )
+}
+
+export default Nav;
